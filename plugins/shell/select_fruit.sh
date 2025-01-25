a1e3a575911bce9d36f26687b100f6b7f89d8bc9
ann <miny9501@naver.com>
2025-01-25 19:08:07 +0900

    Add plugins folder

---
+++ plugins/shell/select_fruit.sh
@@ -0,0 +1,10 @@
+FRUIT=$1   # 첫 번째 입력변수 
+if [ $FRUIT == APPLE ];then
+        echo "You selected Apple!"
+elif [ $FRUIT == ORANGE ];then
+        echo "You selected Orange!"
+elif [ $Fruit == GRAPE ];then
+        echo "You selected Grape!"
+else
+        echo "You selected other Fruit!"
+fi
